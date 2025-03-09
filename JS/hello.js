969bd06a9d2d109614a5b789cbcdd6a4c1fbc4d7
haideraaftab1 <154618454+haideraaftab1@users.noreply.github.com>
2025-03-09 15:22:21 +0530

    first JS

---
+++ JS/hello.js
@@ -0,0 +1,19 @@
+
+
+function printHaider() {
+    console.log("hello haider");
+}
+printHaider();
+
+
+function bringBrush(how) {
+    console.log(`many ${how} brush`);
+}
+
+bringBrush(4);
+
+function sum(n1,n2) {
+    return(n1 + n2);
+}
+
+console.log(sum(1,2));
