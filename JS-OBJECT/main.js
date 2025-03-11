1b21a06e1204916dfa8613594536620e00afae43
haideraaftab1 <154618454+haideraaftab1@users.noreply.github.com>
2025-03-11 23:53:28 +0530

    objects

---
+++ JS-OBJECT/main.js
@@ -0,0 +1,15 @@
+const person = {
+    x:10,
+    fName: 'Haider',
+    lNmae: 'Aftab',
+    hobbies: ['coding', 'gym'],
+    isMarried: false,
+    gfn: function() {
+        return 'Haider Aftab'
+    },
+    address: {
+        hno: 1,
+        street: 1,
+    }
+}
+co
