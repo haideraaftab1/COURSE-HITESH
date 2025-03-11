a03f55e2491cb7d5ef5eabc785150a3c0af05753
haideraaftab1 <154618454+haideraaftab1@users.noreply.github.com>
2025-03-11 23:54:14 +0530

    objects in js

--- JS-OBJECT/main.js
+++ JS-OBJECT/main.js
@@ -12,4 +12,47 @@ const person = {
         street: 1,
     }
 }
-co
+const person = {
+    x:10,
+    fName: 'Haider',
+    lNmae: 'Aftab',
+    hobbies: ['coding', 'gym'],
+    isMarried: false,
+    gfn: function() {
+        return 'Haider Aftab'
+    },
+    address: {
+        hno: 1,
+        street: 1,
+    }
+}
+console.log(person.address.street)
+
+const remote = {
+    color: 'black',
+    brand: 'CYZ',
+    dimesions: {height: 1, width: 1},
+    turnOff: function() {
+
+    },
+}
+
+let fname = 'haider'
+let fname2 = fname 
+
+console.log(fname)
+
+fname2 = 'ali'
+
+console.log(fname)
+
+let p1 = {
+    fname: 'haider'
+}
+
+let p2 = p1
+
+p2.fname = 'ali'
+
+console.log(p2)
+console.log(p1)
